cca30e663ecd8267448b62e303595fecc5fbb3ff
Viktor Kronvall <viktor.kronvall@gmail.com>
2015-09-01 20:40:13 +0200

    Use soft tabs pl0x.

--- labb1/index/main.cpp
+++ labb1/index/main.cpp
@@ -5,25 +5,25 @@
 #include "gen_index.h"
 
 /**
-	Example usage: ./index infile
+    Example usage: ./index infile
 */
 
 int main(int argc, char const *argv[]) {
-	std::ios::sync_with_stdio(false);
-	if(argc != 2) {
-		std::cerr << "Filename required" << std::endl;
-		return EXIT_FAILURE;
-	}
-	// Input file name.
-	std::string in_name = argv[1];
-	// filehandler.
-	std::ifstream in(in_name,std::ios::in);
-	if(in) {
-		generateIndex(in);
-	}else {
-		std::cerr << "File does not exist" << std::endl;
-		return EXIT_FAILURE;
-	}
-	in.close();
-	return EXIT_SUCCESS;
+    std::ios::sync_with_stdio(false);
+    if(argc != 2) {
+        std::cerr << "Filename required" << std::endl;
+        return EXIT_FAILURE;
+    }
+    // Input file name.
+    std::string in_name = argv[1];
+    // filehandler.
+    std::ifstream in(in_name,std::ios::in);
+    if(in) {
+        generateIndex(in);
+    }else {
+        std::cerr << "File does not exist" << std::endl;
+        return EXIT_FAILURE;
+    }
+    in.close();
+    return EXIT_SUCCESS;
 }
--- labb1/main.cpp
+++ labb1/main.cpp
@@ -40,95 +40,95 @@ std::vector<std::string> split(const std::string &s, char delim) {
 const unsigned char letters[] = {'a', 'e','h','l','o','s','t'};
 
 const char getFileName(unsigned char first) {
-	int i;
-	for(i = 0; first >= letters[i] && i < 7; ++i) {}
-	return letters[i-1];
+    int i;
+    for(i = 0; first >= letters[i] && i < 7; ++i) {}
+    return letters[i-1];
 }
 
 int main(int argc, char const *argv[]) {
-	std::ios::sync_with_stdio(false);
-	if(argc != 3) {
-		std::cerr << "Filename required" << std::endl;
-		return EXIT_FAILURE;
-	}
-	std::string dictionary_name = argv[1];
-	std::ifstream dictionary(dictionary_name,std::ios::in);
+    std::ios::sync_with_stdio(false);
+    if(argc != 3) {
+        std::cerr << "Filename required" << std::endl;
+        return EXIT_FAILURE;
+    }
+    std::string dictionary_name = argv[1];
+    std::ifstream dictionary(dictionary_name,std::ios::in);
 
-	std::string file_glob = argv[2];
-	auto files = glob(file_glob);
-	std::string word;
-	while (std::getline(std::cin, word)) {
-		clock_t begin = clock();
+    std::string file_glob = argv[2];
+    auto files = glob(file_glob);
+    std::string word;
+    while (std::getline(std::cin, word)) {
+        clock_t begin = clock();
 
-		std::transform(word.begin(), word.end(), word.begin(), ::tolower);
-		// filehandler.
-		char first = word[0];
-		char c = getFileName(first);
-		std::string file_name = "";
-		for(auto glob_filename : files) {
-			if(glob_filename[0] == c) {
-				file_name = glob_filename;
-				break;
-			}
-		}
-		if(file_name == "") {
-			std::cerr << "Index file not found!" << std::endl;
-			dictionary.close();
-			return EXIT_FAILURE;
-		}
-		 
-		std::ifstream in(file_name,std::ios::in);
-		if(!in) {
-			std::cerr << "Could not open file" << std::endl;
-			dictionary.close();
-			return EXIT_FAILURE;
-		}
-		std::string line;
-		std::string result;
-		std::string search = word+" ";
-		bool found = false;
-		while (std::getline(in, line)) {
-			if(line.find(search) == 0) {
-				//Found!
-				int start = search.size();
-				std::string poss = line.substr(start);
-				auto positions_str = split(poss, ' ');
-				//Convert to ints
-				std::set<int> positions;
-				for(std::string pos : positions_str) {
-					int p = atoi(pos.c_str());
-					positions.insert(p);
-				}
-				const int length = 30;
-				// allocate memory:
-				char * buffer = new char [length];
-				for(int pos : positions) {
-					//Before
-					int before = pos-30;
-					dictionary.seekg(std::max(before,0));		
-				    dictionary.read (buffer,length);
-				    strncpy(buffer, buffer, dictionary.gcount());
-				    std::cout << buffer << " ";
-				    //After
-				    int after = pos+word.size();
-					dictionary.seekg(after);
-					dictionary.read (buffer,length);
-					strncpy(buffer, buffer, dictionary.gcount());
-					std::cout << buffer << std::endl;
-				}
-				delete[] buffer;
-				found = true;
-				break;
-			}
-		}
-		if(!found) {
-			std::cerr << "Word not found!" << std::endl;
-		}
-		clock_t end = clock();
-		double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-		std::cout << elapsed_secs << "s" << std::endl;
-		in.close();
-	}
-	dictionary.close();
-	return EXIT_SUCCESS;
+        std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+        // filehandler.
+        char first = word[0];
+        char c = getFileName(first);
+        std::string file_name = "";
+        for(auto glob_filename : files) {
+            if(glob_filename[0] == c) {
+                file_name = glob_filename;
+                break;
+            }
+        }
+        if(file_name == "") {
+            std::cerr << "Index file not found!" << std::endl;
+            dictionary.close();
+            return EXIT_FAILURE;
+        }
+
+        std::ifstream in(file_name,std::ios::in);
+        if(!in) {
+            std::cerr << "Could not open file" << std::endl;
+            dictionary.close();
+            return EXIT_FAILURE;
+        }
+        std::string line;
+        std::string result;
+        std::string search = word+" ";
+        bool found = false;
+        while (std::getline(in, line)) {
+            if(line.find(search) == 0) {
+                //Found!
+                int start = search.size();
+                std::string poss = line.substr(start);
+                auto positions_str = split(poss, ' ');
+                //Convert to ints
+                std::set<int> positions;
+                for(std::string pos : positions_str) {
+                    int p = atoi(pos.c_str());
+                    positions.insert(p);
+                }
+                const int length = 30;
+                // allocate memory:
+                char * buffer = new char [length];
+                for(int pos : positions) {
+                    //Before
+                    int before = pos-30;
+                    dictionary.seekg(std::max(before,0));
+                    dictionary.read (buffer,length);
+                    strncpy(buffer, buffer, dictionary.gcount());
+                    std::cout << buffer << " ";
+                    //After
+                    int after = pos+word.size();
+                    dictionary.seekg(after);
+                    dictionary.read (buffer,length);
+                    strncpy(buffer, buffer, dictionary.gcount());
+                    std::cout << buffer << std::endl;
+                }
+                delete[] buffer;
+                found = true;
+                break;
+            }
+        }
+        if(!found) {
+            std::cerr << "Word not found!" << std::endl;
+        }
+        clock_t end = clock();
+        double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+        std::cout << elapsed_secs << "s" << std::endl;
+        in.close();
+    }
+    dictionary.close();
+    return EXIT_SUCCESS;
 }
